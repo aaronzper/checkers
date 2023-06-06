e67856c3f768b78d8d6ae1fa7726f49a205b835e
Aaron P <19561067+aaronzper@users.noreply.github.com>
2023-06-06 12:09:01 -0400

    Piece killing

--- src/actor.rs
+++ src/actor.rs
@@ -64,6 +64,43 @@ impl Actor {
                             else {
                                 board.state[piece.x as usize][piece.y as usize].piece = None;
                                 board.state[chosen_move.x as usize][chosen_move.y as usize].piece = Some(self.side);
+
+                                let bigger_x;
+                                let smaller_x;
+                                let bigger_y;
+                                let smaller_y;
+                                if piece.x > chosen_move.x {
+                                    bigger_x = piece.x;
+                                    smaller_x = chosen_move.x;
+                                }
+                                else {
+                                    bigger_x = chosen_move.x;
+                                    smaller_x = piece.x;
+                                }
+                                if piece.y > chosen_move.y {
+                                    bigger_y = piece.y;
+                                    smaller_y = chosen_move.y;
+                                }   
+                                else {
+                                    bigger_y = chosen_move.y;
+                                    smaller_y = piece.y;
+                                }
+
+                                // Remove enemy pieces
+                                for x in smaller_x..bigger_x {
+                                    for y in smaller_y..bigger_y {
+                                        if piece.x.abs_diff(chosen_move.x as u8) == piece.y.abs_diff(chosen_move.y as u8) {
+                                            let other_team = match self.side {
+                                                Side::Red => Side::Blue,
+                                                Side::Blue => Side::Red
+                                            };
+                                            if board.state[x as usize][y as usize].piece == Some(other_team) {
+                                                board.state[x as usize][y as usize].piece = None;
+                                            }
+                                        }
+                                    }
+                                }
+
                                 board.draw().await.unwrap();
                                 break;
                             }
