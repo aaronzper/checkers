5ea58f0eaf7a977381a6be028e013ddc74f5f567
Aaron P <19561067+aaronzper@users.noreply.github.com>
2023-06-13 12:28:02 -0400

    Add terminal args for game settings

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod point;
 mod side;
 mod piece;
 
+use std::env::args;
 use std::io::stdout;
 use crossterm::Result;
 use actor::ActorType;
@@ -11,11 +12,33 @@ use actor::RecursiveActorType;
 use side::Side;
 use game::Game;
 
+fn str_to_actor(input: String) -> Result<ActorType> {
+    match input.to_uppercase().as_str() {
+        "S" => Ok(ActorType::Recursive(RecursiveActorType::Random)),
+        "R" => Ok(ActorType::Random),
+        "H" => Ok(ActorType::Human),
+        _ => Err(crossterm::ErrorKind::new(std::io::ErrorKind::InvalidData, "Invalid actor type code"))
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut game = Game::new(8, 8, Some(stdout()))?;
-    let result = game.play(ActorType::Recursive(RecursiveActorType::Random), ActorType::Human).await.unwrap().unwrap();
-    drop(game);
+    let mut args = args(); // Save a copy of the args
+    args.next(); // Discard the first arg since its just the command
+    let width = args.next().expect("Please provide a width").parse().expect("Please provide a valid width");
+    let height = args.next().expect("Please provide a height").parse().expect("Please provide a valid height");
+
+    if width < 3 { panic!("Width must be at least 3"); }
+    if height < 7 { panic!("Height must be at least 7"); }
+
+    let red_actor_str = args.next().expect("Please provide an actor type for the Red player ([h]uman, [r]andom, or [s]mart)");
+    let blue_actor_str = args.next().expect("Please provide an actor type for the Blue player ([h]uman, [r]andom, or [s]mart)");
+    let red_actor = str_to_actor(red_actor_str).unwrap();
+    let blue_actor = str_to_actor(blue_actor_str).unwrap();
+
+    let mut game = Game::new(width, height, Some(stdout()))?;
+    let result = game.play(red_actor, blue_actor).await.unwrap().unwrap();
+    drop(game); // Drop the game object to restore the terminal to normal
 
     println!("{:?}", result);
 
